e179a853828dd68fe264cf0c3e20303ec497d627
Вероника Тумакова <vt7722@yandex.ru>
2022-05-14 15:58:34 +0600

    Update

--- src/main/kotlin/connect.kt
+++ src/main/kotlin/connect.kt
@@ -3,7 +3,7 @@ import org.litote.kmongo.KMongo
 import org.litote.kmongo.json
 
 val client = KMongo
-    .createClient("mongodb://root:V7dk2c3NNaM1@192.168.172.193:27017")
+    .createClient("mongodb://root:V7dk2c3NNaM1@192.168.161.193:27017")
 val mongoDatabase = client.getDatabase("Groups")
 
 fun prettyPrintJson(json: String) =
